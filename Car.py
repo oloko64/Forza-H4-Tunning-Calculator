b68506a282064dca44e7099eb26f1cb8e33b803d
OLoko64 <naldoepingo1@gmail.com>
2021-04-30 10:57:12 -0300

    Bug fixes

--- Car.py
+++ Car.py
@@ -10,21 +10,22 @@ class Carro:
         return round(final, 2)
 
     def suspencaoRear(self):
-        final = (self.peso * (self.rig_mola / 100)) * ((100 - self.distr_peso) / 100)
-        return round(final, 2)
-
-    def dampingFront(self):
-        final = self.barra_20 * (self.distr_peso / 100)
-        return round(final, 2)
-
-    def dampingRear(self):
-        final = self.barra_20 * ((100 - self.distr_peso) / 100)
+        final = (self.peso * (self.rig_mola / 100)) * \
+            ((100 - self.distr_peso) / 100)
         return round(final, 2)
 
     def reboundFront(self):
-        final = self.dampingFront() * 0.5
+        final = self.barra_20 * (self.distr_peso / 100)
         return round(final, 2)
 
     def reboundRear(self):
-        final = self.dampingRear() * 0.5
+        final = self.barra_20 * ((100 - self.distr_peso) / 100)
+        return round(final, 2)
+
+    def bumpFront(self):
+        final = self.reboundFront() * 0.5
+        return round(final, 2)
+
+    def bumpRear(self):
+        final = self.reboundRear() * 0.5
         return round(final, 2)
--- FH4_Calculator.py
+++ FH4_Calculator.py
@@ -1,8 +1,10 @@
-# Code made by OLoKo64
-
+import Car
 import os
 
+# Code made by OLoKo64
 # Copied code ready to use for clear the console in Unix and Windows machines
+
+
 def clearConsole():
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
@@ -10,126 +12,41 @@ def clearConsole():
     os.system(command)
 
 
-# Global variable for the % of weight distribution of the car
-weight_distr = 0
-
-# Set the weight distribution global variable
-def weight_distribution():
-    global weight_distr
-    weight_distr = 0
-    print(f'>>>> Weight Distribution <<<<')
-    print('')
-    weight_distr = input("Type the % of weight distribution: ")
-    if (weight_distr == ''): # Set to zero if the user press enter without inserting any value
-        weight_distr = 0
-    return weight_distr
-
-
-# Calculates the formula (max - min) * W% + min = result for front and rear
-def calculos():
-    aux = inputs()
-    resul_front = (float(aux[1]) - float(aux[2])) * \
-        (float(aux[0]) / 100) + float(aux[2])
-    resul_back = (float(aux[1]) - float(aux[2])) * \
-        ((100 - float(aux[0])) / 100) + float(aux[2])
-    return (round(resul_front, 2)), (round(resul_back, 2)) # Return the result rounded
-
-
-# ======================
-# Start the inputs
-# ======================
-def inputs():
-    global weight_distr
-    maxi = input("Type the max value of the bar: ")
-    mini = input("Type the min value of the bar: ")
-    if (maxi == '' or mini == ''): # Set to zero if the user press enter without inserting any value
-        maxi = 0
-        mini = 0
-    print('')
-    return weight_distr, maxi, mini
-
-
-def run_weight():
-    weight_distribution()
-    clearConsole()
-
-
-def run_damping():
-    print('>>>> Damping Input <<<<')
-    print('')
-    damping(calculos())
-    print('')
-    input('Press ENTER for the next menu...')
-    clearConsole()
-
-
-def run_springs():
-    print('>>>> Springs Input <<<<')
-    print('')
-    springs(calculos())
-    print('')
-    input('Press ENTER for the next menu...')
-    clearConsole()
-
-
-def run_antiroll():
-    print('>>>> Antiroll Bars Input <<<<')
-    print('')
-    anti_roll(calculos())
-    print('')
-    input('Press ENTER to exit...')
-    clearConsole()
-# ======================
-# End the inputs
-# ======================
-
-
-# ======================
-# Start functions to print the results
-# ======================
-def damping(valor):
-    print(f'>>>> Damping Results <<<<')
-    print('')
-    print(f'Rebound Stiffness Front ==> {valor[0]}')
-    print(f'Rebound Stiffness Rear ==> {valor[1]}')
-    print('')
-    print(f'Bump Stiffness Front ==> {round((valor[0] * 60) / 100, 2)}') # Use 60% of the above values
-    print(f'Bump Stiffness Rear ==> {round((valor[1] * 60) / 100, 2)}')  # Use 60% of the above values
-
-
-def springs(valor):
-    print(f'>>>> Springs Results <<<<')
-    print('')
-    print(f'Springs Front ==> {valor[0]}')
-    print(f'Springs Rear ==> {valor[1]}')
-
-
-def anti_roll(valor):
-    print(f'>>>> Antirolls Bars Results <<<<')
-    print('')
-    print(f'Antiroll Bars Front ==> {valor[0]}')
-    print(f'Antiroll Bars Rear ==> {valor[1]}')
-# ======================
-# End functions to print the results
-# ======================
-
-
-# Call all of the functions and treat errors if the user use invalid parameters
 def run():
     try:
-        run_weight()
-        run_damping()
-        run_springs()
-        run_antiroll()
-    except (ValueError): # If the user use a char instead of a number
-        clearConsole()
-        print('Only numbers are allowed...')
-        print('')
-        run()
-    except: # Formatting in case the user use Ctrl+C
-        clearConsole()
-        print('Exiting...')
+        print('>>>>>>>>>>>>>>> Forza Horizon 4 Tunning Calculator <<<<<<<<<<<<<<<\n\n')
 
-# Run the code
-if __name__ == "__main__":
-    run()
+        carro.peso = float(input('Weight (Kg): '))
+        carro.distr_peso = int(input('Weight distribuition (%): '))
+        if (carro.distr_peso < 0 or carro.distr_peso > 100):
+            raise Exception('Outside of parameters')
+        carro.rig_mola = int(input('Spring stiffness [Default: 20]: ') or 20)
+        carro.barra_20 = int(input('Total damping bar [Default: 20]: ') or 20)
+
+        print('\n')
+
+        print(f'Suspension Front: {carro.suspencaoFront()}')
+        print(f'Suspension Rear: {carro.suspencaoRear()}')
+        print('')
+        print(f'Damping Front: {carro.reboundFront()}')
+        print(f'Damping Rear: {carro.reboundRear()}')
+        print('')
+        print(f'Rebound Front: {carro.bumpFront()}')
+        print(f'Rebound Rear: {carro.bumpRear()}')
+        print('\n')
+    except(ValueError):
+        clearConsole()
+        print('Only numbers are valid...\n')
+        run()
+    except(KeyboardInterrupt):
+        clearConsole()
+        print('Exiting...\n')
+    except(Exception):
+        clearConsole()
+        print('Percentage error... [Only 0 to 100 is accepted]\n')
+        run()
+
+
+carro = Car.Carro(0, 0, 0, 0)
+clearConsole()
+run()
